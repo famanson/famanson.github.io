9ad83f054047f03ab041513a004e4011721ef5e7
Son Pham <me@famanson.com>
2015-01-14 16:38:16 +0000

    Add some data

--- pravo-demo/js/controllers.js
+++ pravo-demo/js/controllers.js
@@ -3,7 +3,7 @@ var app = angular.module('Pravo', ['ngSanitize', 'NgSwitchery']);
 
 app.controller("ListingCtrl", function($scope) {
     // Determine how big each card should be
-    var postWidth = ($(".listing-column").innerWidth()/3 | 0) - 10;
+    var postWidth = ($(".listing-column").innerWidth()/3 | 0) - 30;
     $scope.postWidth = postWidth;
     $scope.postWidthPx = postWidth + "px";
     // Assign listings
--- pravo-demo/js/data.js
+++ pravo-demo/js/data.js
@@ -3,5 +3,35 @@ var listings = [
         'id': 0,
         'price': '10,000,000',
         'bedrooms': 2
+    },
+    {
+        'id': 1,
+        'price': '20,000,000',
+        'bedrooms': 2
+    },
+    {
+        'id': 2,
+        'price': '30,000,000',
+        'bedrooms': 2
+    },
+    {
+        'id': 3,
+        'price': '20,000,000',
+        'bedrooms': 2
+    },
+    {
+        'id': 4,
+        'price': '20,000,000',
+        'bedrooms': 2
+    },
+    {
+        'id': 5,
+        'price': '20,000,000',
+        'bedrooms': 2
+    },
+    {
+        'id': 6,
+        'price': '20,000,000',
+        'bedrooms': 2
     }
 ]
