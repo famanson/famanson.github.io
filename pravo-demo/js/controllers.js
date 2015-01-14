87d378386691c5a3c753cba1129792bef0aed6e1
Son Pham <me@famanson.com>
2015-01-14 16:18:01 +0000

    Calculate post width

--- pravo-demo/js/controllers.js
+++ pravo-demo/js/controllers.js
@@ -3,6 +3,9 @@ var app = angular.module('Pravo', ['ngSanitize', 'NgSwitchery']);
 
 app.controller("ListingCtrl", function($scope) {
     // Determine how big each card should be
+    var postWidth = ($(".listing-column").innerWidth()/3 | 0) - 10;
+    $scope.postWidth = postWidth;
+    $scope.postWidthPx = postWidth + "px";
     // Assign listings
     $scope.listings = listings;
 })
