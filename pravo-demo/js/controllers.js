efeb26bcebf51c2893c61cd2adfa041730d28e29
Son Pham <son.pham@ocado.com>
2015-01-14 20:01:52 +0000

    Add more styling for listings

--- pravo-demo/js/controllers.js
+++ pravo-demo/js/controllers.js
@@ -3,7 +3,10 @@ var app = angular.module('Pravo', ['ngSanitize', 'NgSwitchery']);
 
 app.controller("ListingCtrl", function($scope) {
     // Determine how big each card should be
-    var postWidth = ($(".listing-column").innerWidth()/4 | 0) - 30;
+    var postWidth = ($(".listing-column").innerWidth()/3 | 0) - 15;
+    if ($("body").innerWidth() > 1600) {
+        postWidth = ($(".listing-column").innerWidth()/4 | 0) - 15;
+    }
     $scope.postWidth = postWidth;
     $scope.postWidthPx = postWidth + "px";
     // Assign listings
