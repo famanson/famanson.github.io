d8bb6e6b0ec073dc69cad90b07ca719a99bf54e8
Son Pham <son.pham@ocado.com>
2015-01-15 18:52:21 +0000

    Remove a whole bunch of crap

--- pravo-demo/js/directives.js
+++
@@ -1,7 +0,0 @@
-app.directive('onLastPreview', function() {
-    return function(scope, element, attrs) {
-        if (scope.$last) setTimeout(function(){
-            scope.$emit('lastPreviewCallback', element, attrs);
-        }, 1);
-    };
-});
